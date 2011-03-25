cf19115581a13e26bac4d6abf0bbf1622890dd30
msmolins <msmolins@sigma.ug.edu.pl>
2011-03-26 00:19:32 +0100

    mapki tvShows

--- labNosql/tvShows/csvTOjsonConvert.py
+++ labNosql/tvShows/csvTOjsonConvert.py
@@ -1,11 +1,11 @@
 import csv
 import json
 
-f = open( 'movies3.csv', 'r' )
-reader = csv.DictReader( f, fieldnames = ( "name", "program_creator", "air_date_of_first_episode", "air_date_of_final_episode", "number_of_episodes", "episode_running_time", "country_of_origin", "genre", "currently_in_production") )
+f = open( 'movie.csv', 'r' )
+reader = csv.DictReader( f, fieldnames = ("name", "program_creator", "air_date_of_first_episode", "air_date_of_final_episode", "number_of_episodes", "episode_running_time", "country_of_origin", "genre", "currently_in_production") )
 #out = json.dumps( [ row for row in reader ] )
 #print out
-file = open('tvShows.json', 'w')
+file = open('movie.json', 'w')
 file.write('{"docs":')
 file.write(json.dumps( [ row for row in reader ] ))
 file.write('}')
